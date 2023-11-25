6057bac07709c8f82379ecaadf6cfc99b76990ea
Swapnil Bhattacharyya <swapnilbhyya@gmail.com>
2023-11-25 18:35:21 +0530

    charts for report using pyplot added

--- back-end/generatePDF.py
+++ back-end/generatePDF.py
@@ -1,43 +1,83 @@
 import os
 import sys
+import matplotlib.pyplot as plt
 import database as db
 
-def generateLineChart(data , location):
+def generateLineChart(data, location):
+    # Extract expenses by date
+    expenses_by_date = {}
+    for expense in data:
+        date = expense['date']
+        if date not in expenses_by_date:
+            expenses_by_date[date] = []
+        expenses_by_date[date].append(expense)
 
-    return True
+    # Prepare data for line chart
+    dates = list(expenses_by_date.keys())
+    total_expenses = [sum(expense['amount'] for expense in expenses) for expenses in expenses_by_date.values()]
 
-def generatePieChart(data , location):
+    # Generate line chart
+    plt.figure(figsize=(10, 6))
+    plt.plot(dates, total_expenses)
+    plt.xlabel('Date')
+    plt.ylabel('Total Expense')
+    plt.title('Line Chart of Expenses')
+    plt.grid(True)
+    plt.savefig(location)
+    plt.close()
 
-    return True
+def generatePieChart(data, location):
+    # Extract expenses by category
+    categories = ['food', 'transport', 'entertainment', 'shopping', 'miscellaneous', 'gift', 'investment',
+                   'education', 'healthcare', 'insurance', 'tax', 'rent', 'utilities']
+    expenses_by_category = {category: 0 for category in categories}
 
+    for expense in data:
+        category = expense['category']
+        expenses_by_category[category] += expense['amount']
+
+    # Prepare data for pie chart
+    labels = list(expenses_by_category.keys())
+    expenses = list(expenses_by_category.values())
+
+    # Generate pie chart
+    plt.figure(figsize=(8, 8))
+    plt.pie(expenses, labels=labels, autopct='%1.1f%%')
+    plt.title('Pie Chart of Expenses')
+    plt.axis('equal')  # Equal aspect ratio ensures that the pie chart is circular.
+    plt.savefig(location)
+    plt.close()
 
 def generatePDF(username):
-    oneMonth = get_expenses(username, 1)
-    threeMonths = get_expenses(username, 3)
-    sixMonths = get_expenses(username, 6)
-    oneYear = get_expenses(username, 12)
-    if oneMonth is None:
+    # Get expenses for different periods
+    one_month = get_expenses(username, 1)
+    three_months = get_expenses(username, 3)
+    six_months = get_expenses(username, 6)
+    one_year = get_expenses(username, 12)
+
+    # Check if any error occurred while fetching expenses
+    if one_month is None or three_months is None or six_months is None or one_year is None:
         return False, "Error while getting expenses from MySQL"
-    if threeMonths is None:
-        return False, "Error while getting expenses from MySQL"
-    if sixMonths is None:
-        return False, "Error while getting expenses from MySQL"
-    if oneYear is None:
-        return False, "Error while getting expenses from MySQL"
+
+    # Generate line and pie charts for each period
     try:
+        generateLineChart(one_month, './latex/oneMonthLineChart.png')
+        generatePieChart(one_month, './latex/oneMonthPieChart.png')
 
-        generateLineChart(oneMonth , './latex/oneMonthLineChart.png')
-        generatePieChart(oneMonth , './latex/oneMonthPieChart.png')
-        generateLineChart(threeMonths , './latex/threeMonthsLineChart.png')
-        generatePieChart(threeMonths , './latex/threeMonthsPieChart.png')
-        generateLineChart(sixMonths , './latex/sixMonthsLineChart.png')
-        generatePieChart(sixMonths , './latex/sixMonthsPieChart.png')
-        generateLineChart(oneYear , './latex/oneYearLineChart.png')
-        generatePieChart(oneYear , './latex/oneYearPieChart.png')
+        generateLineChart(three_months, './latex/threeMonthsLineChart.png')
+        generatePieChart(three_months, './latex/threeMonthsPieChart.png')
 
-        os.system('pdflatex ./latex/report.tex')
-        
-        return True , None
+        generateLineChart(six_months, './latex/sixMonthsLineChart.png')
+        generatePieChart(six_months, './latex/sixMonthsPieChart.png')
 
+        generateLineChart(one_year, './latex/oneYearLineChart.png')
+        generatePieChart(one_year, './latex/oneYearPieChart.png')
+    except Exception as e:
+        print(e)
+        return False, "Error while generating charts"
 
-        
+    # Generate PDF report
+    os.system('pdflatex ./latex/report.tex')
+
+    return True, None
+
